bc6344ab2f08a9b9169db581609e85cf3231e6ce
Nicolas Pineda <nicholas.pineda87@gmail.com>
2020-06-07 08:07:55 -0500

    Add Record Routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const authRouter = require("./modules/auth/auth.route.js");
 const userRouter = require("./modules/users/user.route.js");
-const authMiddleware = require("./middleware");
+const recordRouter = require("./modules/records/record.route.js");
 
 const router = express.Router();
+const authMiddleware = require("./middleware");
 
 router.use("/auth", authRouter);
 router.use("/users", authMiddleware, userRouter);
+router.use("/records", authMiddleware, recordRouter);
 
 module.exports = router;
